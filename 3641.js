7374a623d695f1194afe4b3f5a48ad711597386a
Gorizory <sobenin.g@gmail.com>
2017-08-16 22:53:07 +0300

    3641 redone

--- 3641.js
+++ 3641.js
@@ -1,20 +1,26 @@
+'use strict'
+
 function wrap(str, num) {
-  var strFinal = '';
-  var length = str.length;
-  var start = 0;
+  let strFinal = '';
+  let length = str.length;
+  let start = 0;
+
   while (length > 0) {
-    var numTemp = num;
-    for (var i = 1; i < num / 4; i++) {
+    let numTemp = num;
+    for (let i = 1; i < num / 4; i++) {
       if (str[start + num - i - 1] === ' ') {
         numTemp = num - i;
         break;
       }
     }
     strFinal += str.substr(start, numTemp);
-    strFinal += '\n'
+    if (length > numTemp) {
+      strFinal += '\n'
+    }
     length -= numTemp;
     start += numTemp;
   }
+
   return strFinal;
 }
 
