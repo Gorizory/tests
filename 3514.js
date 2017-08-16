f3fc3f5ea45209f57a6b10478fdb237511daf385
Gorizory <sobenin.g@gmail.com>
2017-08-16 22:25:31 +0300

    3514 redone

--- 3514.js
+++ 3514.js
@@ -1,27 +1,31 @@
-var arr1 = [20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5];
-var arr2 = [1,1,2,-2,5,2,4,4,-1,-2,5]
+'use strict'
+
+const arr1 = [20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5];
+const arr2 = [1,1,2,-2,5,2,4,4,-1,-2,5]
+
 function find(arr){
-  function sortNum(a, b) {
+  let newArr = Array.from(arr);
+  newArr.sort((a, b) => {
     if (a > b) {
       return 1;
     } else {
       return -1;
     }
-  }
-  arr.sort(sortNum);
-  var prev = arr[0];
-  var flag = false;
-  for (var i = 0; i < arr.length; i++) {
-    if (prev === arr[i]) {
+  });
+  let prev = newArr[0];
+  let flag = false;
+  for (let i = 0; i < newArr.length; i++) {
+    if (prev === newArr[i]) {
       flag = !flag;
     } else {
       if (flag) {
         return prev;
       }
-      prev = arr[i];
+      prev = newArr[i];
       flag = true;
     }
   }
 }
+
 console.log(find(arr1));
 console.log(find(arr2));
